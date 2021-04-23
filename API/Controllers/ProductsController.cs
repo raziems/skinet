46f50acc667b489f0762adf764a7ca8c3f9c9437
Razie <razie_matsaman@jabil.com>
2021-04-23 11:07:48 +0800

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
-using Infrastructure.Data;
-using Core.Entities;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -12,26 +13,39 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
-        //Endpoint
         [HttpGet]
-        //This is unsync request, this request will be block until is finish
-        //public ActionResult<List<Product>> GetProducts()
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await  _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
+
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return  await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());//need to return Ok() because directly return the value due to IReadOnlyList
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());//need to return Ok() because directly return the value due to IReadOnlyList
+        }
+
+        
     }
 }
